14c91fbb43f60ca51254a0935f843f2d2583bc3a
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2025-09-05 00:04:30 +0530

    JavaScript

---
+++ 01variable.js
@@ -0,0 +1,9 @@
+const accountId = 2007
+let accountEmail = "himanshu@gmail.com"
+var accountPassword = "1980"
+accountCity = "Chennai"
+accountEmail = "baggahimanshu@gmail.com"
+accountCity = "Delhi"
+// prefer not to use var because of in block scope and functional scope
+console.table([accountId , accountEmail , accountPassword , accountCity])
+
