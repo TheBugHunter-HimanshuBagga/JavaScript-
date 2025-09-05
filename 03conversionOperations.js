7c80c1b2975bee6729a8349fe46e3c00be7e93f3
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2025-09-05 11:12:52 +0530

    JavaScript

--- 03conversionOperations.js
+++ 03conversionOperations.js
@@ -47,3 +47,19 @@ let someNumber = 33
 let stringNumber = String(someNumber);
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+console.log(" ")
+
+let number = 3
+let negativeValue = -number
+console.log(negativeValue)
+
+let str1 = "Hello" 
+let str2 = " Bro"
+let str3 = str1 + str2 
+console.log(str3)
+
+console.log("1" + 2) // 12
+console.log(1 + "2") // 12
+console.log("1" + 2 + 2) // 122
+console.log(1 + 2 + "2") // 32
