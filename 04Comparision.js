20623772ca13302b4bd134975026b649c310b7b2
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2025-09-05 11:30:18 +0530

    JavaScript

---
+++ 04Comparision.js
@@ -0,0 +1,11 @@
+console.log(2 > 1) // true
+console.log(2 != 1) // true
+
+// don't do these type of conversions that have 2 diff types of data to compare
+// console.log("2" > 1) 
+// console.log("02" > 1)
+
+//Instead use ===
+
+console.log("2" == 2) // true
+console.log("2" === 2) // false
