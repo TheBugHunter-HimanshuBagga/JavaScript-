ed3c061e33d529e2a99758a6ac06cc5875d4985b
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2025-09-05 10:57:10 +0530

    JavaScript

---
+++ 03conversionOperations.js
@@ -0,0 +1,49 @@
+let score1 = 18
+console.log(typeof score1)
+
+let score2 = "18"
+console.log(typeof score2)
+
+let score3 = "18"
+console.log(typeof Number(score3)) 
+
+let score4 = "25"
+let valueInNumber = Number(score4)
+console.log(typeof valueInNumber)
+
+let score5 = null
+console.log(typeof score5)
+
+let score6 = undefined
+console.log(typeof score6)
+
+let score7 = true // 1
+console.log(typeof score7)
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1 , false => 0
+
+let isLoggedIn = 0 // false , 1=> true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let isLoggedIn1 = "" // false
+
+let booleanIsLoggedIn1 = Boolean(isLoggedIn1)
+console.log(booleanIsLoggedIn1)
+
+let isLoggedIn2 = "Himanshu" // true
+
+let booleanIsLoggedIn2 = Boolean(isLoggedIn2)
+console.log(booleanIsLoggedIn2)
+
+// 1 => true and 0 => false
+// "" => false
+// "Himanshu" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber)
+console.log(typeof stringNumber)
